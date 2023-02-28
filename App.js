4258203253ac2f426893f4707d6386a0dfb52a8e
paframar <paframar@gmail.com>
2023-02-28 20:20:33 -0300

    Header, Input, Card added.

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from '@react-native-material/core'
+import Header from './components/Header';
+import Card from './components/Card';
+import Input from './components/Input'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <>
+      <Header />
+      <View style={styles.container}>
+        <Card>
+            <Text style={styles.text}>Elija un numero</Text>
+            <Input/>
+            <View style={styles.buttonContainer}>
+              <Button title={'Limpiar'} onPress={()=>{}}/>
+              <Button title={'Confirmar'} onPress={()=>{}}/>
+            </View>
+        </Card>      
+      </View>
+  </>
   );
 }
 
+
+
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    padding:20,
+    borderWidth:1,
+    width:'100%',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center',
+    alignContent:'center',
   },
+  buttonContainer:{
+    width:'100%',
+    flexDirection:'row',
+    justifyContent:'space-around',
+    alignContent:'center',
+  },
+  text:{
+    fontSize:18,
+  }
+  
 });
