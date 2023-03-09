7281969eb24405a0f6c1e3b40df614e27016d319
paframar <paframar@gmail.com>
2023-03-09 18:51:03 -0300

    Desafío: Switch entre pantalla.

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
+
+import { useState, useCallback } from 'react'
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  const [userNumber, setUserNumber] = useState()
+
+  const handleGameStart = selectedNumber => {
+    setUserNumber(selectedNumber)
+  }
+
+
+  const styles = StyleSheet.create({
+    container:{
+      height:'100%'
+    },
+  })
+
+  if (!fontsLoaded) return null;
+
   return (
-    <>
+    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
         <Header/>
-        <StartGameScreen/>
-    </>
+        {
+          !userNumber
+          ? <StartGameScreen onGameStart={handleGameStart}/>
+          : <GameScreen />
+        }
+    </SafeAreaView>
   );
 }
